3867ca652c4a51c95e56a9d924d797d111140603
marcinperek <marcinp05@outlook.com>
2026-01-26 19:28:25 +0100

    notebook.display_tensors add option to rescale normalized images

--- src/utils/notebook.py
+++ src/utils/notebook.py
@@ -26,14 +26,16 @@ def imshow(a: np.ndarray[Any, np.dtype[np.uint8]]) -> None:
     display(Image.fromarray(a))
 
 
-def display_tensor(img: Tensor) -> None:
+def display_tensor(img: Tensor, rescale=True) -> None:
     """
     Display an image from a PyTorch tensor in Jupyter Notebook.
 
     :param img: Input image tensor
     :type img: Tensor
+    :param rescale: Whether to rescale the image from [-1, 1] or [0, 1] to [0, 255], defaults to True
+    :type rescale: bool, optional
     """
-    imshow(image_from_tensor(img))
+    imshow(image_from_tensor(img, rescale=rescale))
 
 
 def display_batch(batch: Tensor | list) -> None:
@@ -51,3 +53,25 @@ def display_batch(batch: Tensor | list) -> None:
     batch = batch[:25]
     grid = make_grid(batch, padding=4, normalize=True)
     imshow(image_from_tensor(grid))
+
+def reload_module(target: Any) -> Any:
+    """
+    Reload a module or a function's module.
+    
+    If target is a function, returns the reloaded function.
+    If target is a module, returns the reloaded module.
+    
+    Example:
+    display_tensor = reload_module(display_tensor)
+    """
+    import sys
+    import importlib
+    from inspect import isfunction
+    
+    if isfunction(target):
+        module = sys.modules[target.__module__]
+        func_name = target.__name__
+        importlib.reload(module)
+        return getattr(module, func_name)
+    else:
+        return importlib.reload(target)
